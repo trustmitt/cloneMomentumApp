32fb0e8b5e96342a89f10b2b68a6b8215e758ee5
mitt <111720709+trustmitt@users.noreply.github.com>
2022-10-22 15:41:02 +0900

    Create background.js

---
+++ js/background.js
@@ -0,0 +1,10 @@
+const images = ["00.jpg", "01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg", "06.jpg", "07.jpg", "08.jpg"];
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImg = document.createElement("img");
+bgImg.src = `img/${chosenImage}`;
+
+bgImg.classList.add("hidden");
+document.body.style.backgroundImage = `url(${bgImg.src})`;
+
+document.body.appendChild(bgImg);
